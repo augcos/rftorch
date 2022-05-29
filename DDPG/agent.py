626b26ec3a102b870263f2523940680e3ed940d9
augcos <augcos@protonmail.com>
2022-05-30 00:50:32 +0200

    major corrections in DDPG

--- DDPG/agent.py
+++ DDPG/agent.py
@@ -5,34 +5,38 @@ from memory import Memory
 import copy
 
 # DQN_agent is the class for the agent implementing a Deep-Q-Network agent
-class DQN_agent():
-    def __init__(self, actor_network, critic_network, tau=250, gamma=0.99, mem_size=100000, 
-                batch_size=64, action_limit=1):        
+class DDPG_Agent():
+    def __init__(self, actor_network, critic_network, tau=0.001, gamma=0.99, mem_size=1000000, batch_size=64):        
         self.actor_network = actor_network                      # actor neural network
         self.critic_network = critic_network                    # critic neural network        
         self.target_actor = copy.deepcopy(actor_network)        # target network is a copy of the main network
         self.target_critic = copy.deepcopy(critic_network)      # target network is a copy of the main network
                           
-        self.tau = tau                              # target networks update period
+        self.tau = tau                              # target networks update constant
         self.gamma = gamma                          # reward discount rate
         self.batch_size = batch_size                # training batch size
-        self.action_limit = action_limit            # value limit of the action space
+        self.device = self.actor_network.device
 
         self.noise = OUActionNoise(mu=np.zeros(self.actor_network.n_actions))               # noise generator
-        self.memory = Memory(mem_size=mem_size, input_shape=actor_network.input_shape)      # memory object of the agent
-
+        self.memory = Memory(mem_size=mem_size, input_dim=actor_network.input_dims,         # memory object of the agent
+                                action_dim=actor_network.n_actions)      
 
     # get_action returns the action for a given state with the highest q-value
-    def get_action(self, state, train=False):
+    def get_eval_action(self, state):
         self.actor_network.eval()
-        state = T.tensor(state).to(self.actor_network.device)
-        mu = self.actor_network.forward(state).to(self.actor_network.device)
-        if train:
-            mu_prime = mu + T.tensor(self.noise(), dtype=T.float).to(self.actor_network.device)
-        else:
-            mu_prime = mu
+        state = T.tensor(state, dtype=T.float).to(self.device)
+        mu = self.actor_network.forward(state).to(self.device)
         
-        return mu_prime.cpu().detach().numpy()
+        return mu.cpu().detach().numpy()
+
+    # get_action returns the action for a given state with the highest q-value
+    def get_train_action(self, state):
+        self.actor_network.eval()
+        state = T.tensor(state, dtype=T.float).to(self.device)
+        mu = self.actor_network.forward(state).to(self.device)
+        mu_prime = mu + T.tensor(self.noise(), dtype=T.float).to(self.device)
+        
+        return mu_prime
 
 
     # learn executes a learning step for the neural network of the agent
@@ -41,55 +45,63 @@ class DQN_agent():
         if self.memory.mem_cntr < self.batch_size:
             return
 
-        self.actor_network.optimizer.zero_grad()        # network gradients are zeroed out
-        self.critic_network.optimizer.zero_grad()
-        self.update_target_network()                    # target networks update function is called
-
         # memory is sampled and the outputs turned into pytorch tensors
-        states, actions, rewards, new_states, dones = self.memory.sample_memory(batch_size=self.batch_size)
-        states = T.tensor(states, dtype=T.float).to(self.actor_network.device)
-        actions = T.tensor(actions, dtype=T.float).to(self.actor_network.device)
-        rewards = T.tensor(rewards, dtype=T.float).to(self.actor_network.device)
-        new_states = T.tensor(new_states, dtype=T.float).to(self.actor_network.device)
-        dones = T.tensor(dones, dtype=T.int).to(self.actor_network.device)
-        
+        states, actions, rewards, new_states, dones = self.memory.sample_memory(
+                                                        batch_size=self.batch_size, device=self.device)
+      
         # networks used for loss calculations are turned to eval mode
+        self.actor_network.eval() 
+        self.critic_network.eval() 
         self.target_actor.eval() 
         self.target_critic.eval()  
-        self.critic_network.eval() 
 
-        # values are computed
-        target_actions = self.target_actor.forward(new_states)
-        target_new_critic_values = self.target_critic.forward(new_states, target_actions)
-        critic_values = self.critic_network.forward(states, actions)
+        # q
+        mu = self.actor_network.forward(states)
+        q = self.critic_network.forward(states, actions)
+        mu_prime = self.target_actor.forward(new_states)
+        q_prime = self.target_critic.forward(new_states, mu_prime)
 
-        # target values are computed
-        target = []
+        q_target = []
         for i in range(self.batch_size):
-            target.append(rewards[i] + self.gamma * target_new_critic_values[i] * dones[i])
-        target = T.tensor(target).to(self.actor_network.device)
-
-
+            q_target.append(rewards[i] + self.gamma * q_prime[i] * dones[i])
+        q_target = T.tensor(q_target, dtype=T.float).to(self.device)
+        q_target = q_target.view(self.batch_size, 1)
+        
+        # Critic network is updated
         self.critic_network.train()
         self.critic_network.optimizer.zero_grad()
-        critic_loss = T.nn.Functional.mse_loss(target, critic_values)
+        critic_loss = T.nn.functional.mse_loss(q_target, q)
         critic_loss.backward()
         self.critic_network.optimizer.step()
         self.critic_network.eval()
 
-        mu = self.actor_network.forward(states)
-        self.actor_network.train()
+        # Actor network is updated
+        self.actor_network.train() 
+        self.actor_network.optimizer.zero_grad()            # network gradients are zeroed out
         actor_loss = -self.critic_network.forward(states, mu)
         actor_loss = T.mean(actor_loss)
         actor_loss.backward()
         self.actor_network.optimizer.step()
 
+        self.update_target_networks()
+
 
     # update_target_network updates the target network parameters with the parameters of the 
     # main network (every tau learning steps)
-    def update_target_network(self):
-        if self.memory.mem_cntr % self.tau == 0:
-            self.target_actor.load_state_dict(self.actor_network.state_dict())
-            self.target_critic.load_state_dict(self.critic_network.state_dict())
+    def update_target_networks(self):
+        actor_params = dict(self.actor_network.named_parameters())
+        critic_params = dict(self.critic_network.named_parameters())
+        target_actor_params = dict(self.target_actor.named_parameters())
+        target_critic_params = dict(self.target_critic.named_parameters())
+
+        for name in actor_params:
+            target_actor_params[name] = self.tau*actor_params[name].clone() + \
+                (1-self.tau)*target_actor_params[name].clone()
+
+        for name in critic_params:
+            target_critic_params[name] = self.tau*critic_params[name].clone() + \
+                (1-self.tau)*target_critic_params[name].clone()
 
 
+        self.target_actor.load_state_dict(target_actor_params)
+        self.target_critic.load_state_dict(target_critic_params)
--- DDPG/memory.py
+++ DDPG/memory.py
@@ -3,12 +3,12 @@ import numpy as np
 
 # Memory is the class for a memory buffer
 class Memory():
-    def __init__(self, mem_size, input_shape):
+    def __init__(self, mem_size, input_dim, action_dim):
         self.mem_size = mem_size
-        self.state_memory = np.zeros((self.mem_size, *input_shape), dtype=np.float32)
-        self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
+        self.state_memory = np.zeros((self.mem_size, *input_dim), dtype=np.float32)
+        self.action_memory = np.zeros((self.mem_size, action_dim), dtype=np.float32)
         self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
-        self.new_state_memory = np.zeros((self.mem_size, *input_shape), dtype=np.float32)
+        self.new_state_memory = np.zeros((self.mem_size, *input_dim), dtype=np.float32)
         self.done_memory = np.zeros(self.mem_size, dtype=np.bool)
         self.mem_cntr = 0
     
@@ -20,18 +20,18 @@ class Memory():
         self.action_memory[index] = action
         self.reward_memory[index] = reward
         self.new_state_memory[index] = new_state
-        self.done_memory[index] = done
+        self.done_memory[index] = 1-done
         self.mem_cntr += 1
 
     # sample_memory returns a batch of memories with size batch_size
-    def sample_memory(self, batch_size):
+    def sample_memory(self, batch_size, device):
         batch_index = np.random.choice(min(self.mem_cntr, self.mem_size),
                                         batch_size, replace=False)
 
-        states = self.state_memory[batch_index]
-        actions = self.action_memory[batch_index]
-        rewards = self.reward_memory[batch_index]
-        new_states = self.new_state_memory[batch_index]
-        dones = self.done_memory[batch_index]
+        states = T.tensor(self.state_memory[batch_index], dtype=T.float).to(device)
+        actions = T.tensor(self.action_memory[batch_index], dtype=T.float).to(device)
+        rewards = T.tensor(self.reward_memory[batch_index], dtype=T.float).to(device)
+        new_states = T.tensor(self.new_state_memory[batch_index], dtype=T.float).to(device)
+        dones = T.tensor(self.done_memory[batch_index], dtype=T.bool).to(device)
         
         return states, actions, rewards, new_states, dones
